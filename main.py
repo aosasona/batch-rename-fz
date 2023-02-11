bb36b2176e0f666c35357bab70aad88f4aa4cb99
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-02-11 22:14:54 +0000

    Added argument parsing and safety to prevent the script from being used on the same file twice and stripping the name entirely

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 import os
+import sys
+
+"""
+When you download things from some sites, they come with annoying names with equally annoying suffixes 
+This script removes _ (underscore) and the weird suffix from ALL the files in a directory or from a single file
+"""
 
 
 def rename(path: str):
-    path = path.strip("/")
     for file in os.scandir(path):
         if file.is_dir() == True:
             rename(file.path)
             continue
+
         ext = file.name.split(".").pop()
         filename_arr = file.name.split("_")
+
+        if len(filename_arr) <= 1:
+            print("Nothing to rename here... exiting now")
+            exit(0)
+
         new_filename = " ".join(filename_arr[:len(filename_arr)-1]) + f".{ext}"
         src, dest = f"{path}/{file.name}", f"{path}/{new_filename}"
         os.rename(src, dest)
-        print(f"--> Renamed {src} to {dest}")
+        print(f"[done] {file.name} --> {new_filename}")
 
     print("All done!")
 
 
 def main():
-    path = input("Enter the path to the target directory: ")
+    args = sys.argv
 
-    print(f"Attempting to read path: {path}")
+    if len(args)>= 2:
+        path = args[1]
+    else:
+        path = input("Enter the path to the target directory: ")
+
+
+    if path.startswith("~"):
+        path = os.path.expanduser(path)
+
+    print(f"Target --> {path}")
 
     if os.path.exists(path) == False:
         print("File does not exist")
-        exit()
+        exit(1)
 
     rename(path)
 
